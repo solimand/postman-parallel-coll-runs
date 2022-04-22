b00c5c432396da32b059f3184dff832e5427ff4b
Michele <michele.solimando@unibo.it>
2022-04-22 17:00:54 +0200

    added CLI params

--- pm-parallel-collection-runs.js
+++ pm-parallel-collection-runs.js
@@ -3,38 +3,44 @@
  */
 var path = require('path'), // ensures that the path is consistent, regardless of where the script is run from
 
-    async = require('async'), // https://npmjs.org/package/async
-    newman = require('newman'), // change to require('newman'), if using outside this repository
+async = require('async'), // https://npmjs.org/package/async
+newman = require('newman'), // change to require('newman'), if using outside this repository
 
-    /**
-     * A set of collection run options for the paralle collection runs. For demonstrative purposes in this script, an
-     * identical set of options has been used. However, different options can be used, so as to actually run different
-     * collections, with their corresponding run options in parallel.
-     *
-     * @type {Object}
-     */
-    options = {
-        reporters: 'cli',
-        collection: process.argv[2]
-        //"https://www.getpostman.com/collections/763e53b7eb53f5439c28"
-        //path.join(__dirname, 'DeepMon-MW-test.postman_collection.json')
-    },
+/**
+ * A set of collection run options for the paralle collection runs. For demonstrative purposes in this script, an
+ * identical set of options has been used. However, different options can be used, so as to actually run different
+ * collections, with their corresponding run options in parallel.
+ *
+ * @type {Object}
+ */
+options = {
+    reporters: 'cli',  // TODO try Json      
+    collection: process.argv[2]
+    // iterationCount: 2,
+    // iterationData: process.argv[2],
+},
 
-    /**
-     * A collection runner function that runs a collection for a pre-determined options object.
-     *
-     * @param {Function} done - A callback function that marks the end of the current collection run, when called.
-     */
-    parallelCollectionRun = function (done) {
-        newman.run(options, done);
-    };
+/**
+ * A collection runner function that runs a collection for a pre-determined options object.
+ *
+ * @param {Function} done - A callback function that marks the end of the current collection run, when called.
+ */
+parallelCollectionRun = function (done) {
+    newman.run(options, done);
+};
 
 // Runs the Postman sample collection thrice, in parallel.
-async.parallel([
-    parallelCollectionRun,
-    parallelCollectionRun,
-    parallelCollectionRun
-],
+var runArray = new Array();
+for(var i=0; i<process.argv[3]; i++){
+        runArray.push(parallelCollectionRun)
+}
+async.parallel(runArray,
+
+
+// async.parallel([
+//     parallelCollectionRun,
+//     parallelCollectionRun
+// ],
 
 /**
  * The
